56984327164c56ccd87a9b34bd64dd66c0af354c
key-moon <kymn0116@gmail.com>
2025-05-02 19:36:14 +0900

    a

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="libc-tools",
+    name="shlib-tools",
     version="0.0.1",
     install_requires=[],
     extras_require={
     },
     entry_points={
         "console_scripts": [
-            "prepare-lib = prepare_lib.main:main",
             "extract-lib = extract_lib.main:main",
             "patchlib = patchlib.main:main",
         ]
