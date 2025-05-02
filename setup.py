e982ab5c11390ead672ad5e27fd27b6420fc1542
key-moon <kymn0116@gmail.com>
2025-05-02 19:38:17 +0900

    do src layout

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
     name="shlib-tools",
     version="0.0.1",
     install_requires=[],
-    extras_require={
-    },
+    extras_require={},
     entry_points={
         "console_scripts": [
             "extract-lib = extract_lib.main:main",
